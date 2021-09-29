bfaa41434e126f278a69f2b82c1ef7e18a396d3b
Divya Kelaskar <divyakelaskar@gmail.com>
2021-09-29 19:17:48 +0530

    First commit

---
+++ Assets/Scripts/InitButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InitButton : MonoBehaviour {
+    private GameObject lastSelect;
+
+	// Use this for initialization
+	void Start () {
+        lastSelect = new GameObject();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EventSystem.current.currentSelectedGameObject == null) {
+            EventSystem.current.SetSelectedGameObject(lastSelect);
+        }
+        else {
+            lastSelect = EventSystem.current.currentSelectedGameObject;
+        }
+	}
+}
